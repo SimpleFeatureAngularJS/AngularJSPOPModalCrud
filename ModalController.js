642c5b1652324044e6794a4ac4f33922ad7b6906
Phon Kunthai <kunthai.corarl@gmail.com>
2017-09-16 16:31:42 +0700

    add style

--- ModalController.js
+++ ModalController.js
@@ -17,3 +17,19 @@ app.controller('ModalController',['$scope',function($scope){
    };
 
 }]);
+
+app.directive('fileModel', ['$parse', function ($parse) {
+    return {
+       restrict: 'A',
+       link: function(scope, element, attrs) {
+          var model = $parse(attrs.fileModel);
+          var modelSetter = model.assign;
+          
+          element.bind('change', function(){
+             scope.$apply(function(){
+                modelSetter(scope, element[0].files[0]);
+             });
+          });
+       }
+    };
+ }]);
